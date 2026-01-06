bc453622460c3c7f4835d891495bd641121c4bb1
Drake <dariocostarelli88@gmail.com>
2026-01-06 13:14:29 +0000

    Add missing allauth and CKEditor 5 URL includes

--- elysium_archive/urls.py
+++ elysium_archive/urls.py
@@ -1,20 +1,25 @@
-"""URL configuration for elysium_archive project."""
-
+from django.contrib import admin
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("home.urls")),
-    path("accounts/", include("accounts.urls")),  
-    path("accounts/", include("allauth.urls")), 
+    path("accounts/", include("allauth.urls")),         
+    path("accounts/", include("accounts.urls")),         
     path("archive/", include("products.urls")),
     path("cart/", include("cart.urls")),
     path("checkout/", include("checkout.urls")),
+    path("ckeditor5/", include("django_ckeditor_5.urls")),  
+    path("", include("home.urls")),
 ]
 
+# Handler per errori
+handler400 = "django.views.defaults.bad_request"
+handler403 = "django.views.defaults.permission_denied"
+handler404 = "django.views.defaults.page_not_found"
+handler500 = "django.views.defaults.server_error"
+
+# Media files in DEBUG
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
